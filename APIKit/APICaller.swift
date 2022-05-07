de99689d6b003b9c02f76b7f04ada77c8eaed9f7
MichaelNovosad <m.novosad@artk.ai>
2022-05-07 22:42:09 +0300

    1. Add API caller framework 2. Add CoursesViewController framework 3. Add both dependencies into main vc

---
+++ APIKit/APICaller.swift
@@ -0,0 +1,44 @@
+//
+//  APICaller.swift
+//  APIKit
+//
+//  Created by Michael Novosad on 07.05.2022.
+//
+
+import Foundation
+
+
+
+
+public class APICaller {
+    public static let shared = APICaller()
+    
+    private init() {}
+ 
+    public func fetchCourses(completion: @escaping ([String]) -> Void) {
+        guard let url = URL(string: "https://iosacademy.io/api/v1/courses/index.php") else {
+            completion([])
+            return }
+        let session = URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data, error == nil else {
+                completion([])
+                return
+            }
+            
+            //Decode
+            do {
+                guard let json = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed) as?  [[String: String]] else {
+                    completion([])
+                    return
+                }
+                
+                let names: [String] = json.compactMap({ $0["name"] })
+                completion(names)
+            }
+            catch {
+                completion([])
+            }
+        }
+        session.resume()
+    }
+}
--- DI/ViewController.swift
+++ DI/ViewController.swift
@@ -4,16 +4,31 @@
 //
 //  Created by Michael Novosad on 07.05.2022.
 //
-
+import APIKit
+import MyAppUIKit
 import UIKit
+
+extension APICaller: DataFetchable { }
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //https://iosacademy.io/api/v1/courses/index.php
+        
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 250, height: 50))
+        view.addSubview(button)
+        button.backgroundColor = .systemBlue
+        button.setTitle("Tap me", for: .normal)
+        button.center = view.center
+        button.setTitleColor(.white, for: .normal)
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
     }
-
+    @objc
+    private func didTapButton() {
+        let vc = CoursesViewController(dataFetchable: APICaller.shared)
+        present(vc, animated: true)
+    }
 
 }
 
